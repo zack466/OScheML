adbf99b972e5c115aca9d435f3c176ba4d655837
Zachary Huang <zack466@gmail.com>
2021-09-08 23:23:32 -0400

    sqrt example working

--- examples/sqrt.scm
+++ examples/sqrt.scm
@@ -6,6 +6,18 @@
 
 ; (define dx 0.0001)
 
+(define (abs x)
+  (if (< x 0) (- x) x))
+
 (define (sqrt x)
-  (
-   ))
+  (define (good-enough? y dx)
+    (> dx (abs (- x (* y y)))))
+  (define (improve guess)
+    (* 0.5 (+ guess (/ x guess))))
+  (define (rec guess)
+    (if (good-enough? guess 0.0001) guess (rec (improve guess)))
+   )
+  (rec 1.0)
+  )
+
+(sqrt 1234)
