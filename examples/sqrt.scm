4d451106152266f5583382fe089cc558e0fae0d1
Zachary Huang <zack466@gmail.com>
2021-11-01 09:43:48 -0400

    uncomment lines

--- examples/sqrt.scm
+++ examples/sqrt.scm
@@ -1,10 +1,8 @@
 ; calculates square roots using newton's method
 
-; (define y 1.0) ; initial guess
-
-; (define x 100.0) ; square of target
-
-; (define dx 0.0001)
+(define y 1.0) ; initial guess
+(define x 100.0) ; square of target
+(define dx 0.0001)
 
 (define (abs x)
   (if (< x 0) (- x) x))
@@ -15,10 +13,8 @@
   (define (improve guess)
     (* 0.5 (+ guess (/ x guess))))
   (define (rec guess)
-    (if (good-enough? guess 0.0001) guess (rec (improve guess)))
-   )
-  (rec 1.0)
-  )
+    (if (good-enough? guess 0.0001) guess (rec (improve guess))))
+  (rec 1.0))
 
 (sqrt 1234)
 
